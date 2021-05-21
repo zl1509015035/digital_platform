4c41adc1a8e5c3490797678063f38d2a9c100423
zl1509015035 <1509015035@qq.com>
2021-05-21 19:13:06 +0800

    Initial commit

---
+++ src/main/java/com/it/web/WebApplication.java
@@ -0,0 +1,14 @@
+package com.it.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+        System.out.println("-----run success-----");
+    }
+
+}
